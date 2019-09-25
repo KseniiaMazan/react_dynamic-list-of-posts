fbeba51937e873843f1834940f28a0678a434637
KseniiaMazan <ksusamazan@gmail.com>
2019-09-25 16:34:22 +0300

    Updates

---
+++ precache-manifest.bd6eeb4a16416b1288cf872baa0b4eea.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "689a0ad870375b0352975fb6d6f4c024",
+    "url": "/react_dynamic-list-of-posts/index.html"
+  },
+  {
+    "revision": "086e44e137eec9639e83",
+    "url": "/react_dynamic-list-of-posts/static/css/main.e092cb72.chunk.css"
+  },
+  {
+    "revision": "a9eee651abe68f4afff6",
+    "url": "/react_dynamic-list-of-posts/static/js/2.ddf4bddb.chunk.js"
+  },
+  {
+    "revision": "086e44e137eec9639e83",
+    "url": "/react_dynamic-list-of-posts/static/js/main.2129d03c.chunk.js"
+  },
+  {
+    "revision": "fc988510490290486853",
+    "url": "/react_dynamic-list-of-posts/static/js/runtime~main.69709ad2.js"
+  }
+]);
